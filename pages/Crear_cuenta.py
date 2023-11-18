2d543aa2152ae47ec85b1355030e3438c937bd2d
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2023-11-18 15:39:02 -0500

    Update Crear_cuenta.py

--- pages/Crear_cuenta.py
+++ pages/Crear_cuenta.py
@@ -225,7 +225,13 @@ else:
             st.write(f"La película '{pelicula_a_borrar}' ha sido borrada de las favoritas.")
             st.write(df_cuenta_actual)
             st.write(listado_peliculas)
-                #df_cuenta_actual.to_csv("cuenta_actual.csv", index=False)
-                #df_cuentas.to_csv("cuentas.csv", index = False)
+            if correo in df_cuentas['Correo'].values:
+                df_cuentas.loc[df_cuentas['Correo'] == correo, 'Peliculas Favoritas'] = listado_peliculas
+                st.write(
+            "<span style='color:red; font-weight:bold;'>Las peliculas favoritas han sido cambiadas con exito</span>",
+            unsafe_allow_html=True,
+        )
+                df_cuenta_actual.to_csv("cuenta_actual.csv", index=False)
+                df_cuentas.to_csv("cuentas.csv", index = False)
         
 
